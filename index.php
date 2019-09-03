60db5b0ac8221971d8abc292977f740954e17654
zhaoyaoke <zhaoyaobackup@outlook.com>
2019-09-03 09:19:27 +0800

    1

---
+++ index.php
@@ -0,0 +1,463 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<link rel="shortcut icon" href="favicon.ico" />
+		
+		<title>互联网上的跳梁小丑 - 赵耀傻逼事迹整理 - 耀科云|嗑药云|耀科云计算|安徽奕耀|耀讯</title>
+		<meta name="keywords" content="互联网上的跳梁小丑,赵耀,耀科云,嗑药云,耀科云计算,安徽奕耀,耀讯,嗑药云计算"/>
+		<meta name="description" content="互联网上的跳梁小丑，赵耀傻逼事迹整理，让你看看赵耀的各种傻逼事迹。"/>
+		
+		<link href="assets/css/main.css?v=20190826030851" rel="stylesheet">
+	</head>
+	<body>
+		<div class="main">
+			<h1 class="text-center">互联网上的跳梁小丑<br/>赵耀傻逼事迹整理</h1>
+			
+			<div class="logo">
+				<img src="assets/img/pill.png" link=no>
+				<p>巨婴，该嗑药了，顺便请去<a href="http://www.ahmhcentre.com/" target="_blank">合肥四院</a>看一下</p>
+			</div>
+			<div class="text-center">
+				<a href="update.html">更新记录</a> | <a href="zhaoyao.zip" target="_blank">整站下载</a> | <a href="https://web.archive.org/web/*/https://yaoke.cloud/" target="_blank">永久备份</a>
+			</div>
+			
+			<div class="warning">
+				<div class="item">
+					<img src="assets/img/i/wczr.jpg" />
+					<p class="title">持续硬汉</p>
+					<p>在本站公开其傻逼事迹后赵耀仍持续进行硬汉行为。<a href="#cxyh">加大力度</a>，请。</p>
+				</div>
+				<div class="item blue">
+					<img src="assets/img/i/zrkd.png" />
+					<p class="title">欢迎互动</p>
+					<p>每天一笑防抑郁，笔者相信与赵先生互动一定每天都可为各位高雅人士带来乐趣。<a href="#info">点击查看联系方式</a>，请。</p>
+				</div>
+				<div class="item green">
+					<img src="assets/img/i/eye.png" />
+					<p class="title">圣地巡礼</p>
+					<p>该页面涉及人物已亲自光临本站，<a href="#cxyh">并已进行表态</a>。</p>
+				</div>
+				<!-- SO, I'M WAITING FOR YOU.
+				<div class="item green">
+					<img src="assets/img/i/stop.png" />
+					<p class="title">停止迫害</p>
+					<p>
+						考虑此人近期已无恶劣行为，<br/>
+						加之其已进行道歉并退网回归虚无。<br/>
+						故不建议近期对其进行迫害或取乐行为。<br/>
+						若此人仍然进行黑屁或其他恶劣行为，请联系本站邮箱。
+					</p>
+				</div> -->
+			</div>
+			
+			<h1 class="title-d">关于本站</h1>
+			<p>文本中“赵耀”指年龄为20岁的安徽凤阳小岗村啃老废青，现居住于安徽蚌埠。</p>
+			<p>本以为此人已回归虚无，过着天天王者荣耀24h在线在家安心啃老的日子。但是近期此人仍然将自己当做跳梁小丑出现在公众视野中。所以本站开始着手整理此人司马事迹，让更多的人知道此人到底做了哪些司马事情。</p>
+			<p>本站内容来源于多位高雅人士，旨在揭露并记录事实，希望各位以正确态度看待本站，以赵耀为戒，切勿重蹈覆辙，遭人唾弃。</p>
+			
+			<h1 class="title-d">名词解释</h1>
+			<ul id="wordTips">
+				<li><span>巨婴</span>：指虽然已经成年,但心智仍然停留在幼儿阶段,极不成熟的人。<a href="https://baike.baidu.com/item/%E5%B7%A8%E5%A9%B4/23188611" target="_blank">百毒百科</a></li>
+				<li><span>HTML</span>：标记语言，不具备执行逻辑代码的功能，所以无法实现“云计算”。<a href="https://baike.baidu.com/item/HTML/97049" target="_blank">百毒百科</a></li>
+				<li><span>黑屁</span>：指网络上一系列不负责任的发言。广义上的黑屁，泛指一切颠倒事实的言论。常见于黑屁者被扒出精确数据后拒不承认数据的真实性，满嘴跑火车试图转移话题，而技穷后直接发表“来信砍”等暴恐性质的发言。<a href="https://www.baidu.com/s?ie=UTF-8&wd=%E9%BB%91%E5%B1%81" target="_blank">百毒</a> <a href="https://cn.bing.com/search?q=%E9%BB%91%E5%B1%81" target="_blank">Bing</a></li>
+				<li><span>啃老</span>：传统上人们以经济独立作为长大成人的标志，但啃老族生活的主要来源是父母，“长大不成人”是这些人及社会面临的共同尴尬。<a href="https://baike.baidu.com/item/%E5%95%83%E8%80%81/4653485" target="_blank">百毒百科</a></li>
+				<li><span>跳梁小丑</span>：[原意]比喻猖狂捣乱而成不了大气候的坏人。<a href="https://baike.baidu.com/item/%E8%B7%B3%E6%A2%81%E5%B0%8F%E4%B8%91/84999" target="_blank">百毒百科</a> [文中意]比喻做无用功结果只会惹人发笑的精神失常人。</li>
+				<li><span>疼讯举报机制漏洞</span>：任意伪造一些证据即可通过<a href="https://110.qq.com" target="_blank">110.qq.com</a>对任意QQ/微信进行封号。</li>
+				<li><span>二级域名分发平台</span>：一些站长有一些闲置域名，所以使用域名解析的接口将域名分发给公众使用。但是这样有很大的风险，其中一个就是你不知道使用你域名的人是一些什么牛鬼蛇神。</li>
+				<li><span>恶俗</span>：指某种虚假、粗陋、毫无智慧、没有才气、空洞而令人厌恶的东西，但是，有很多人竟错误的相信它们是纯正、高雅、明智或迷人的。<a href="https://baike.baidu.com/item/恶俗/7716157" target="_blank">百毒百科</a></li>
+				<li><span>高雅人士</span>：顾名思义，是与恶俗的人、事、物相对立的人。</li>
+				<li><span>WAF</span>：Web应用防护系统，保护网站不被“底剁死”。<a href="https://baike.baidu.com/item/WAF/3239498" target="_blank">百毒百科</a></li>
+				<li><span>底剁死</span>：DDOS，分布式拒绝服务攻击。原理是占用大量资源导致宕机，从而导致无法正常服务。同理可用于短信、电话、邮件等。<a href="https://baike.baidu.com/item/分布式拒绝服务攻击/3802159" target="_blank">百毒百科</a></li>
+				<li><span>硬汉</span>：[字面意思]坚强不屈的男子。 [文中意]脸皮比城墙还厚，并且以自己做的司马事情为荣。</li>
+				<li><span>大脑升级</span>：说话做事经过大脑，不再进行黑屁以及做出巨婴行为，认真面对自己的历史，敢于承担自己的所作所为，真正意义上的“成年”。</li>
+			</ul>
+			
+			<h1 class="title-d">推荐阅读</h1>
+			<ul>
+				<li><a href="https://www.zhihu.com/question/54783089" target="_blank">知乎：如何评价耀科云？</a></li>
+				<li><a href="https://52huameng.com/tucao/547" target="_blank">华梦博客：震惊！某退学初中生竟独自搭建起HTML云计算还在线转播春晚！</a> <a href="https://web.archive.org/web/20190823195821/https://52huameng.com/tucao/547" target="_blank">备份</a></li>
+				<li><a href="https://52huameng.com/tucao/884" target="_blank">华梦博客：半年前的跳梁小丑，你们是否还记得？</a> <a href="https://web.archive.org/web/20190823195947/https://52huameng.com/tucao/884" target="_blank">备份</a></li>
+				<li><a href="https://t.me/YaoKeCloud" target="_blank">@YaoKeCloud: 耀科云精选黑屁备份</a></li>
+				<li><a href="https://t.me/YaoKeCloudGroup" target="_blank">@YaoKeCloudGroup: 耀科云群聊备份</a></li>
+			</ul>
+			
+			<h1 class="title-d">此人简介</h1>
+			<p>赵耀（1999.09.09~），安徽凤阳县小岗村人。HTML云计算发起者，在不懂得任何相关知识的情况下放出大量关于云计算的黑屁。颅内意淫高手，常在脑内意淫收购阿里云和各种公司。啃老族，高中三年基本家里蹲，结果就是高考分数考到了136分的高分，在问其原因时，声称<b>“我故意不想考好，我不想让中国教育浪费时间，所以我故意考差不上大学。”</b>但是笔者发现其王者荣耀基本24小时在线。中级举报高手，擅长使用疼讯举报机制漏洞对高雅人士进行封号。</p>
+			
+			<h1 class="title-d" id="info">个人信息</h1>
+			<ul>
+				<li>姓名：赵耀</li>
+				<li>年龄：20（巨婴）</li>
+				<li>民族：汉</li>
+				<li>生日：1999.09.09</li>
+				<li>
+					公司：
+					<ul>
+						<li>安徽奕耀科技有限公司（注销）<a href="https://www.tianyancha.com/company/3222816751" target="_blank">天眼查</a></li>
+						<li>蚌埠耀讯信息科技有限公司（续存）<a href="https://www.tianyancha.com/company/3294848386" target="_blank">天眼查</a></li>
+					</ul>
+				</li>
+				<li>
+					QQ:
+					<ul>
+						<li>511880706 (已被橄榄,将于2019/08/26 10:08:20恢复) (主号,欢迎高雅人士进行举报让他王者荣耀封号3QXX)</li>
+						<li>2079034438</li>
+						<li>3168475110</li>
+						<li>1808278611</li>
+						<li>1335129769</li>
+						<li>2609258151</li>
+						<li>168738937</li>
+						<li>3462283577</li>
+					</ul>
+				</li>
+				<li>
+					微信：fyzk73
+				</li>
+				<li>
+					邮件：
+					<ul>
+						<li>168738937@qq.com</li>
+						<li>3462283577@qq.com</li>
+						<li>2079034438@qq.com</li>
+					</ul>
+				</li>
+				<li>
+					手机号码(赵耀多次使用以下*7开头号码骚扰高雅人士,请与其友好沟通,务必不要使用自己的手机进行沟通,可使用网络电话)：
+					<ul>
+						<li>**375339379 (保护隐私隐去前两位)</li>
+						<li>**856304735 (保护隐私隐去前两位)</li>
+					</ul>
+				</li>
+			</ul>
+			
+			<hr/>
+			
+			<h1 class="title-d">事迹</h1>
+			<h2>1. 发家起身</h2>
+			<h3>1.1. 开始降智</h3>
+			<p>耀科云起源于2017年初，当时赵耀18岁，已经是一个成年人。笔者不知道其受到了怎样的良好教育，导致他身为一个成年人却作出诸多巨婴行为。笔者认为其巨婴行为，家庭以及老师的教育不到位有一定的责任。</p>
+			<p>赵耀从某<s>免费二级域名分发平台申请了“yun.yfyfy.cn”</s><sup>内容错误，等待新资料</sup>（后变更为izhaoyao.cc），之后下载了一个html模板进行简单修改后用作其官网（耀科云计算），放置在免费空间里。之后将官网发送到各QQ群进行宣传，由于官网内容太过于草生（如HTML云计算、赵耀本尊头像仍然使用模板内置的示例图片以及其介绍为“主要<span class="big">弄</span>云计算”），引来一大批围观群众加入其QQ群。高雅人士一般称“耀科云计算”为“嗑药云计算”，意思就是这孩子该吃药了。</p>
+			<p>赵耀将其几个小号设置为群管，并设置“CEO”、“COO”等头衔，颇具大厂风范。</p>
+			<p>随后便开始了在群里大肆放黑屁，并且声称“我们耀科云有全网唯一的html实现云计算功能”，精彩言论如下：</p>
+			
+			<ul>
+				<li>“我们耀科云是全球最大的云计算服务平台”</li>
+				<li>“耀科云必将一统宇宙！！”</li>
+				<li>“我们耀科云有全网唯一的html实现云计算功能”</li>
+				<li>“现在互联网谁不知道我们耀科云？”</li>
+				<li>“欢迎体验和使用耀科云的产品，与6.5亿用户一同携手，搭建美好，无限的，安全天空！”</li>
+				<li>“现在全球攻击频繁，ddoS导致网站长期打不开，耀科云看不下去了，因此，耀科云上线高防CDN，1000G硬防，有效抗ddoS!..”</li>
+			</ul>
+			
+			<div class="img">
+				<img src="assets/img/sb/0.jpg" title="性别：女">
+				<img src="assets/img/sb/1.jpg" title="1+1=12">
+				<img src="assets/img/sb/2.jpg" title="嗑药云是我们外号">
+				<img src="assets/img/sb/3.jpg" title="我不是，我没有">
+				<img src="assets/img/sb/4.jpg" title="现在互联网谁不知道我们耀科云">
+				<img src="assets/img/sb/5.jpg" title="真 · 制作服务器">
+				<img src="assets/img/sb/6.jpg" title="我开发了HTML">
+				<img src="assets/img/sb/7.jpg" title="万物基于HTML">
+			</div>
+			
+			<h3>1.2. 我骂我自己</h3>
+			<p>当时正处于QQ推出“口令红包”功能，高雅人士发送了多个调侃耀科云的口令红包，赵耀为了一分钱也进行了抢红包，事后声称“我只是为了抢红包”，可见其经济状况十分不佳，既然如此，又是如何做到收购大厂的呢？</p>
+			
+			<div class="img">
+				<img src="assets/img/hb/0.jpg" title="都是傻逼">
+				<img src="assets/img/hb/1.jpg" title="我糙我自己">
+				<img src="assets/img/hb/2.jpg" title="我是沙雕">
+				<img src="assets/img/hb/3.jpg" title="兼职赵小姐">
+				<img src="assets/img/hb/4.jpg" title="“我只是为了抢红包”">
+			</div>
+			
+			<h3>1.3. 禁言大手子</h3>
+			<p>加入其QQ群的基本都是为了前去调侃。但是赵耀真不傻，他学会了使用身为群主拥有的权限：禁言和出群留学。</p>
+			<p>笔者暂不知其禁言目的性何在，是为了停止吃瓜群众对其调侃？但是赵耀应该知道，加入该群的只有来调侃的吃瓜群众以及高雅人士，应该没有哪个傻子会相信其“HTML云计算”。</p>
+			<div class="img">
+				<img src="assets/img/jy/0.jpg" title="未经授权不可擅关禁言">
+				<img src="assets/img/jy/1.jpg" title="未经“总部”授权不准关">
+				<img src="assets/img/jy/2.jpg" title="耀科耀科闹闹闹">
+				<img src="assets/img/jy/3.jpg" title="出群留学">
+				<img src="assets/img/jy/4.jpg" title="无限开火权">
+			</div>
+			
+			<h2>2. “底剁死”</h2>
+			
+			<p>其言论稍有常识的人都能看出是在放屁，但是黑屁的实在是太过于低级，所以部分高雅人士认为赵耀在故意炒作，将其官网“底剁死”，免费空间也被管理员关闭。</p>
+			<p>赵耀为了让嗑药云官网保持正常运行，更换了空间并使用了陆续使用了多款WAF产品，包括百度云加速、云盾、360WAF，但都被陆续封禁。</p>
+			<p>之后赵耀转用360WAF（360网站卫士，现更名为“<a href="https://wangzhan.qianxin.com/" target="_blank">奇安信网站卫士</a>”）进行防御。在高雅人士向360WAF管理员进行举报后，360WAF官方随即封禁了该网站，并且将其踢出各360WAF交流群，停止此人继续在群内黑屁。</p>
+			
+			<div class="img">
+				<img src="assets/img/ddos/0.jpg" title="迫真等48小时">
+				<img src="assets/img/ddos/1.jpg" title="随便打">
+				<img src="assets/img/ddos/2.jpg" title="静态网站还想D死">
+				<img src="assets/img/ddos/3.jpg" title="迫真整改公告">
+				<img src="assets/img/ddos/5.png" title="360WAF群管证实">
+			</div>
+			
+			<p>在云盾封禁其网站后，赵耀将群名称改为“YUNDUN免费用户群”试图进行混淆云盾用户，随后云盾甚至发出官方公告进行澄清。</p>
+			<p>在云盾发出公告后，赵耀也同时发出公告声称攻击云盾，但是其学历限制了他对于法律的了解。群内吃瓜群众提醒他在进行网络攻击是违法行为，可能会立案后，赵耀大放其词“立你妈逼”，深刻提现其对于法律常识的匮乏。</p>
+			<div class="img">
+				<img src="assets/img/ddos/yd/0.jpg" title="攻击云盾通知">
+				<img src="assets/img/ddos/yd/1.jpg" title="立你妈逼">
+			</div>
+			
+			<h2>3. 直播春晚</h2>
+			
+			<p>赵耀在推出耀科云后，大脑进一步降级，正好赶上过年，于是就有了“耀科云直播2017春晚”。赵耀使用html6编写了一个真 · 辣眼睛的页面并放置在其免费空间上，实际就是在页面内嵌入了优酷的春晚播放页面。</p>
+			<p>但是赵耀当时并不知道，<a href="http://www.wenming.cn/xwcb_pd/yw/201701/t20170125_4030505.shtml" target="_blank">《关于禁止未经授权通过网络传播中央电视台2017年春节联欢晚会的通知》</a>里明确规定<u>除爱奇艺、腾讯、优酷等已获授权，其他未经权利人许可，不得通过直播等服务传播春晚。</u> 也就是说，赵耀的行为实际上就是属于盗链。但是赵耀却一直声称其网站为<b>转播</b>春晚。该行为已经构成侵权。</p>
+			<p>在吃瓜群众提醒后，赵耀迫真使用审查元素修改网页内容并截图，发表说说声称“耀科云计算拿到春晚直播权”。</p>
+			<p>群内高雅人士纷纷举报其侵权行为，导致其官网网址被腾讯报红。事后，耀科云发出了迫真公告，声称是疼讯网站检测系统出现异常。</p>
+			<p>疑似因高雅人士举报，耀科云不得不撤下其官网转播页面，随后继续黑屁，声称“已收购爱奇艺，所以大家在爱奇艺观看即可”。</p>
+			
+			<div class="img">
+				<img src="assets/img/cw/0.jpg" title="直播春晚">
+				<img src="assets/img/cw/1.jpg" title="啊我的眼睛">
+				<img src="assets/img/cw/7.png" title="审·查·元·素">
+				<img src="assets/img/cw/6.jpg" title="热烈庆祝">
+				<img src="assets/img/cw/2.jpg" title="疼讯拦截">
+				<img src="assets/img/cw/3.jpg" title="赔我几千亿">
+				<img src="assets/img/cw/4.jpg" title="迫真取消直播">
+				<img src="assets/img/cw/5.jpg" title="爱奇艺，请">
+			</div>
+			
+			<h2>4. 颅内意淫</h2>
+			<p>由于赵耀自身没有什么能力和技术，既不会写代码也不会做项目，并且也是穷二代一个，所以只好在颅内意淫。经常黑屁其收购了各种公司，躺枪名单（包括但不仅限于）有：</p>
+			<ul>
+				<li><a href="https://www.aliyun.com/" target="_blank">阿里云</a> (云计算服务商)</li>
+				<li><a href="https://www.iqiyi.com/" target="_blank">爱奇艺</a> (因直播春晚)</li>
+				<li><a href="https://www.360.cn/" target="_blank">360</a> (因360WAF封禁其网站,但是360WAF现在成立独立公司)</li>
+				<li><a href="https://www.youku.com/" target="_blank">优酷</a></li>
+				<li><a href="https://www.tencent.com/" target="_blank">疼讯</a></li>
+				<li><a href="https://www.taobao.com/" target="_blank">淘宝</a></li>
+				<li><a href="https://www.yundun.com/" target="_blank">云盾</a> (WAF)</li>
+				<li>第五科技 (IDC,已跑路)</li>
+				<li><a href="https://www.gfvps.cn/" target="_blank">数掘科技</a> (IDC)</li>
+				<li>facebook (脸书)</li>
+			</ul>
+			
+			<div class="img">
+				<img src="assets/img/llyy/0.jpg" title="收购">
+				<img src="assets/img/llyy/1.jpg" title="这种小公司我们不收购">
+				<img src="assets/img/llyy/2.jpg" title="wy.cn: 我不是我没有">
+				<img src="assets/img/llyy/3.jpg" title="腾讯都是我旗下的">
+				<img src="assets/img/llyy/4.jpg" title="facebook入股">
+				<img src="assets/img/llyy/5.jpg" title="微软:fnmdp">
+				<img src="assets/img/llyy/6.jpg" title="微软fb再次躺枪">
+				<img src="assets/img/llyy/7.jpg" title="和跑路云合作">
+				<img src="assets/img/llyy/8.jpg" title="我全都要.jpg">
+				<img src="assets/img/llyy/9.jpg" title="不是不稀罕吗">
+				<img src="assets/img/llyy/10.jpg" title="数掘:wdnmd">
+			</div>
+			
+			<h2>5. 弄云计算</h2>
+			<p>（图片待补充）幻の象：嗑药那个uuugood.com竟然是用我免费空间搭建的.jpg</p>
+			
+			<p>-- 未完待补充 --</p>
+			
+			<h2>6. 车软</h2>
+			
+			<h3>6.1. 律师函狂魔</h3>
+			<p>-- 未完待补充 --</p>
+			
+			<h3>6.2. 删帖！删帖！删帖！</h3>
+				<a href="https://52huameng.com/tucao/884" target="_blank">华梦博客：半年前的跳梁小丑，你们是否还记得？</a>
+				<p>-- 未完待补充 --</p>
+			<h3>6.3. 机房投诉</h3>
+			<p>-- 未完待补充 --</p>
+			
+			<hr/>
+			<h1 class="title-d">近况</h1>
+			
+			<h2>1. 黑屁朋友</h2>
+			<p>-- 未完待补充 --</p>
+			
+			<h2>2. 啃老废物</h2>
+			<p>-- 未完待补充 --</p>
+			
+			<h2>3. 小学生黑客圈</h2>
+			<p>-- 未完待补充 --</p>
+			
+			<h2>4. 国家栋梁</h2>
+			<p>-- 未完待补充 --</p>
+			
+			<h2>5.精虫上脑</h2>
+			<p>-- 未完待补充 --</p>
+			
+			<h2>6. 邮件“底剁死”</h2>
+			<p>-- 未完待补充 --</p>
+			
+			<h2>7. 举报高手</h2>
+			<p>-- 未完待补充 --</p>
+			
+			<h2 id="cxyh">8. 持续硬汉</h2>
+			<h3>8.1. 再次出道</h3>
+			<div class="warning">
+				<div class="item yellow">
+					<img src="assets/img/i/date.png" />
+					<p class="title">当前事件</p>
+					<p>当前事件发生在2019年8月，记录的内容随时可能会有新的改变或成为历史。</p>
+				</div>
+			</div>
+			<p>在本站挖掘其傻逼事迹后，赵耀不但不进行反思，反而按照其一贯黑屁风格声称<u>“干的不错，我们大耀科要火了，我们火便大江南北，哈哈哈哈哈哈哈哈，不错，继续努力！”</u>。</p>
+			<p>事过两年，赵耀先生还未完成大脑升级，其本人仍然放出诸多黑屁，作出诸多巨婴行为，笔者表示惋惜。并建议其尽快到<a href="http://www.ahmhcentre.com/" target="_blank">合肥第四人民医院</a>(笔者亲身体验,医疗条件挺好的)接受治疗。</p>
+			<p>也欢迎各位高雅人士进行包括但不仅限于以下行为：</p>
+			<ul>
+				<li>联系其<b>监护人</b>带它去合肥四院就诊。</li>
+				<li>拨打其电话劝其尽快就诊。</li>
+				<li>举报其王者农药账号(可造成比屠杀亲*还大的打击)。</li>
+				<li>联系本人进行找乐(因其嘴臭,心理承受能力差或有抑郁症等情况不建议)。</li>
+				<li>发送验证码进行底剁死攻击。</li>
+				<li>宣传本站让更多人知道其傻逼事迹。</li>
+			</ul>
+			<div class="img">
+				<img src="assets/img/jdld/0.png" title="邮件底剁死">
+				<img src="assets/img/jdld/1.png" title="火“便”大江南北">
+			</div>
+			
+			<p>另外赵耀发送完邮件后仍然不忘记嘴臭，试图对高雅人士进行精神上的打击，殊不知该高雅人士在观赏了赵耀的跳梁小丑表演后，已经两个月没有出现抑郁情况，活生生被赵先生逗乐了。</p>
+			<div class="img">
+				<img src="assets/img/jdld/2.png" title="soga">
+			</div>
+			
+			<h3>8.2. 迫真警告</h3>
+			<div class="warning">
+				<div class="item yellow">
+					<img src="assets/img/i/date.png" />
+					<p class="title">当前事件</p>
+					<p>当前事件发生在2019年8月，记录的内容随时可能会有新的改变或成为历史。</p>
+				</div>
+			</div>
+			
+			<h3>8.2.1. 小学生发文</h3>
+			<p>赵耀于2019年8月24日晚发出了迫真警告，全文如下：</p>
+			
+			<pre>
+			《关于对郑丽霞等人最后警告》
+			来源：jg.yaocent.com
+			作者：赵耀
+			
+			近期，我们发现互联网上面，有一个叫郑丽霞的人，颠倒黑白，无视法律，无视政府，对赵耀进行恶意刻意诋毁，并在其YAOKE.CLOUD网站上大肆宣传大量虚假内容，网站上面含有大量颠倒黑白，歪曲事实，恶意甚至刻意诋毁，断章取义误导公众社会，如果此人依然我行我素，揭露所谓荒谬事实因此取乐，本公司将会采取一些措施，包括但不限于[DELETE]，特此通告。<span class="big">赵耀是赵耀的赵耀</span>，轮不到你们说三道四，你们还是管好自己的事情吧，赵耀不用你们操心！
+			</pre>
+			
+			<p>从其发表的内容可以看出，高中三年的确是在家啃老，教育水平仍然停留在小学。并且毫无法律常识，<s>“[DELETE]”</s>可能涉嫌寻衅滋事，<s>[DELETE]</s>可能涉及<s>[DELETE]</s>罪。</p>
+			<p>可以看出，赵耀在光临本站后仍未反思其所作所为，继续保持其黑屁风格以及硬汉行为。</p>
+			<p>至于文中提到“并在其YAOKE.CLOUD网站上大肆宣传大量虚假内容，网站上面含有大量颠倒黑白，歪曲事实，恶意甚至刻意诋毁，断章取义误导公众社会”，请各位读者自行明辨是非。</p>
+			
+			<h3>8.2.2. 大脑升级 (?)</h3>
+			<p>赵耀于2019年8月25日早修订了其网站，修正了几处可能引起法律纠纷的地方，试图进行<span class="big">洗白</span>，但仍然被高雅人士当小学生的作文拿去修改病句。</p>
+			<p>此次进步略大，疑似大脑完成了些许升级。<b>但究竟是大脑升级了，还是幕后有吃瓜群众帮助，笔者暂且蒙在鼓里。</b></p>
+			
+			<p>全文如下：(删除线为删除部分,加粗为新增部分)</p>
+			<pre>
+			《关于对郑丽霞等人的最后提醒以及对社会致歉声明》
+			来源：jg.yaocent.com (2019-08-25)
+			作者：赵耀
+			
+			近期，我们发现互联网上面，有一个叫郑丽霞的人，颠倒黑白，无视法律，无视政府，对赵耀进行恶意刻意诋毁，并在其YAOKE.CLOUD网站上大肆宣传大量虚假内容，网站上面含有大量颠倒黑白，歪曲事实，恶意甚至刻意诋毁，断章取义误导公众社会，如果此人依然我行我素，揭露所谓荒谬事实因此取乐，本公司将会采取一些措施，<s>包括但不限于[DELETE]，特此通告</s><b>我们会联系公安机关进行报案，立案调查！特此公告。</b><s>赵耀是赵耀的赵耀</s><b>赵耀是赵耀</b>，轮不到你们说三道四，你们还是管好自己的事情吧，赵耀不用你们操心！<b>你们没有权利，也没有资格！</b>
+			</pre>
+			
+			<h3>8.2.3. 闭站</h3>
+			<p>截止2019年8月6日18时，其网站DNS解析已被删除，网站无法打开。具体原因未知。</p>
+			
+			<div class="img">
+				<img src="assets/img/now/xxzs/3.png" title="迫真公告">
+				<img src="assets/img/now/xxzs/1.jpg" title="试图洗白">
+				<img src="assets/img/now/xxzs/2.jpg" title="小学生改错">
+			</div>
+			
+			<hr/>
+			
+			<h1 class="title-d">采取措施</h1>
+			<p>2019-08-24: 高雅人士成功以其人之道还治其人之身，将其封号，导致其王者荣耀三天无法登录，造成比**去世还大的打击。顺便造福其队友。</p>
+			<div class="img">
+				<img src="assets/img/ban/0.png" title="让你坑队友">
+			</div>
+			
+			<hr/>
+			
+			<h1 class="title-d">噔噔咚 <small>高雅画廊</small></h1>
+			<p>以下内容可能会引起不适，请选择观看。</p>
+			<div class="img">
+				<img src="assets/img/ddd/0.png" title="证件照">
+				<img src="assets/img/ddd/1.png" title="欸电梯真好玩">
+				<img src="assets/img/ddd/2.jpg" title="头发都成尖了还不理">
+				<img src="assets/img/ddd/3.jpg" title="噔噔咚">
+			</div>
+			
+			<hr/>
+			<div class="text-center">
+				Copyright (C) 2019 Yaoke.Cloud | 内容来源于网络 | 异议/投稿/反馈错误:zhaoyaobackup#outlook.com
+			</div>
+		</div>
+		
+		<script src="assets/js/jquery.min.js"></script>
+		<script>
+		$(function(){
+			var wordTips = [];
+			var top = 0;
+			
+			$('#wordTips li span').each(function(){
+				wordTips.push($(this).html());
+				$(this).parent().append('<span class="goback">返回</span>');
+			});
+			
+			$('.img img[link!="no"]').each(function(index, this_){
+				this_.outerHTML = '<div class="imgBox">' + this.outerHTML + '<p>' + ($(this).attr('title') ? $(this).attr('title') : '&nbsp;') + '</p></div>';
+			});
+			
+			$('.imgBox').click(function(){
+				openwindow($('img', $(this)).attr('src'), 'imgViewer', 800, 600);
+			});
+			
+			$('ul[id!="wordTips"] li,ol li,p,h1,h2').each(function(index, this_){
+				$.each(wordTips, function(index2, value){
+					$(this_).html($(this_).html().replace(value, '<span class="tips" title="查看词意" index="' + index2 + '">'+value+'</span>'));
+				});
+			});
+			
+			$('.tips').click(function(){
+				$('#wordTips li').removeClass('active');
+				
+				var dom = $('#wordTips li:nth-child(' + (parseInt($(this).attr('index')) + 1) + ')');
+				
+				top = $('html,body').scrollTop();
+				dom.addClass('active');
+				$('html,body').scrollTop($(dom).offset().top - 100);
+			});
+			
+			$('.goback').click(function(){
+				$('html,body').scrollTop(top);
+			});
+			
+			$('pre').each(function(){
+				$(this).html($(this).html().replace(/\t/g, ''));
+			});
+			
+			function openwindow(url,name,iWidth,iHeight){
+				var url;                            //转向网页的地址;
+				var name;                           //网页名称，可为空;
+				var iWidth;                         //弹出窗口的宽度;
+				var iHeight;                        //弹出窗口的高度;
+				//window.screen.height获得屏幕的高，window.screen.width获得屏幕的宽
+				var iTop = (window.screen.height-30-iHeight)/2;       //获得窗口的垂直位置;
+				var iLeft = (window.screen.width-10-iWidth)/2;        //获得窗口的水平位置;
+				window.open(url,name,'height='+iHeight+',,innerHeight='+iHeight+',width='+iWidth+',innerWidth='+iWidth+',top='+iTop+',left='+iLeft+',toolbar=no,menubar=no,scrollbars=auto,resizeable=no,location=no,status=no');
+			}
+			
+		});
+		</script>
+		<script>
+		var _hmt = _hmt || [];
+		(function() {
+		  var hm = document.createElement("script");
+		  hm.src = "https://hm.baidu.com/hm.js?171461b630896465518364322b8202d1";
+		  var s = document.getElementsByTagName("script")[0]; 
+		  s.parentNode.insertBefore(hm, s);
+		})();
+		</script>
+	</body>
+</html>
